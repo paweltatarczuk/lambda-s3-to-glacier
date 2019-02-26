7a466e000991d4380497b020c866e387135aeb98
Paweł Tatarczuk <paweltatarczuk@gmail.com>
2019-02-26 20:48:56 +0100

    Add files

---
+++ README.md
@@ -0,0 +1,22 @@
+# AWS Lambda function - Duplicity S3 to Glacier
+
+## Description
+
+Simple Lambda function that moves all duplicity files from S3 'STANDARD'
+storage class to 'GLACIER' storage class.
+
+Files are moved if all constraints are met:
+
+- Bigger than 128KB
+- Not ending with `.manifest` or `.manifest.gpg`
+- Storage class is 'STANDARD'
+
+## Environment variables
+
+| Name | Description |
+|-|-|
+| BUCKET_NAME | (Required) Name of the bucket to process |
+
+## License
+
+MIT
---
+++ lambda_function.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python3
+import boto3
+import os
+import re
+
+response = []
+
+bucket = os.environ['BUCKET_NAME']
+
+s3 = boto3.client('s3')
+
+def process_objects(continue_token=''):
+    args = {'Bucket': bucket}
+    if continue_token:
+        args['ContinuationToken'] = continue_token
+    objects = s3.list_objects_v2(**args)
+
+    for obj in objects['Contents']:
+        if obj['StorageClass'] != 'STANDARD':
+            response.append('Skipping %s - not a STANDARD storage class' % (obj['Key'],))
+            continue
+
+        if obj['Size'] <= 128000:
+            response.append('Skipping %s - smaller than 128KB' % (obj['Key'],))
+            continue
+
+        if re.search(r'\.manifest(\.gpg)?$', obj['Key']):
+            response.append('Skipping %s - manifest file' % (obj['Key'],))
+            continue
+
+        response.append('Moving %s to GLACIER storage class' % (obj['Key'],))
+        s3.copy(
+            {
+                'Bucket': bucket,
+                'Key': obj['Key']
+            },
+            bucket,
+            obj['Key'],
+            ExtraArgs = {
+                'StorageClass': 'GLACIER',
+                'MetadataDirective': 'COPY'
+            }
+        )
+
+    if 'NextContinuationToken' in objects:
+        process_objects(objects['NextContinuationToken'])
+
+def lambda_handler(event, context):
+    err = None
+
+    try:
+        process_objects()
+    except Exception as e:
+        err = str(e)
+    return {
+        'status': 500 if err else 200,
+        'lines': response,
+        'error': err
+    }
